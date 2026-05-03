fb2809d6b7176b488db7d03803352372593e9879
gautam606 <gauravsk615@gmail.com>
2026-05-03 13:13:01 +0530

    resolved merge conflict

---
+++ src/areOccurrenceseq.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class areOccurrenceseq {
+    public static void main(String[] args) {
+            int i,sum=0;
+            String s="aaabb";
+            HashSet<Integer> set=new HashSet<>();
+            int n=s.length();
+            if(n%2==0)
+            {
+                System.out.println("true");
+            }
+            else
+            {
+                System.out.println("false");
+            }
+    }
+}
---
+++ src/l1941.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class l1941 {
+
+    public boolean areOccurrencesEqual(String s) {
+       HashMap <Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        int targetFreq = freqMap.get(s.charAt(0));
+
+
+        for (int freq : freqMap.values()) {
+            if (freq != targetFreq) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+
+    public static void main(String[] args) {
+        String s="abacbc";
+//        "aaabb";
+        l1941 l=new l1941();
+        boolean result=l.areOccurrencesEqual(s);
+        System.out.println(result);
+    }
+}
